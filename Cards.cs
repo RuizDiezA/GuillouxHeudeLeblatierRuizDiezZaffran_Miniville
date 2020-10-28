0df7692980343a8be5fc6f447fb7ce80391f1f17
Sacha-Zaffran <73350628+Sacha-Zaffran@users.noreply.github.com>
2020-10-28 11:27:57 +0100

    Update Cards.cs

--- Cards.cs
+++ Cards.cs
@@ -1,10 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Miniville
 {
-    class Cards
+    public class Cards
     {
+        string color;
+        int prix;
+        List<int> CardsValue = new List<int>();
+        public Cards()
+        {
+            CardsValue.Add(1);
+            CardsValue.Add(2);
+            CardsValue.Add(4);
+            CardsValue.Add(6);
+
+            void CheckAndApplyEffect(int prix, string color)
+            {
+                {
+                }
+            }
+        }
     }
 }
