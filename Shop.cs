2fcc9d2348b60a6a03af4c319864985f00d188ad
brikedelai <pleblatier@live.fr>
2020-10-28 14:33:46 +0100

    class Piles (shop)

---
+++ Shop.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Miniville
+{
+    class Shop
+    {
+
+        List<Cards> ChampsBle = new List<Cards>();
+        List<Cards> Ferme = new List<Cards>();
+        List<Cards> Boulangerie = new List<Cards>();
+        List<Cards> Cafe = new List<Cards>();
+        List<Cards> Superette = new List<Cards>();
+        List<Cards> Foret = new List<Cards>();
+        List<Cards> Restaurant = new List<Cards>();
+        List<Cards> Stade = new List<Cards>();
+
+        List<List<Cards>> Shops = new List<List<Cards>>();
+
+        public List<List<Cards>> LigneAchat()
+        {
+            Shops.Add(ChampsBle);
+            Shops.Add(Ferme);
+            Shops.Add(Boulangerie);
+            Shops.Add(Cafe);
+            Shops.Add(Superette);
+            Shops.Add(Foret);
+            Shops.Add(Restaurant);
+            Shops.Add(Ferme);
+            Shops.Add(Stade);
+
+            return Shops;
+        }
+
+    }
+
+    
+}
