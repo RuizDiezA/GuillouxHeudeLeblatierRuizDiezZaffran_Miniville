c6bb70ea5c0e1b78656d5468fd0fab4f2190bd4e
AlexisGuilloux <alexis.guilloux.pro@gmail.com>
2020-10-28 11:02:49 +0100

    Test de commit et push pour Die.cs

--- Die.cs
+++ Die.cs
@@ -6,5 +6,36 @@ namespace Miniville
 {
     class Die
     {
+
+        public int value;
+
+        public Die()
+        {
+            Random rand = new Random();
+
+
+
+        }
+
+
+        public int Lancer()
+        {
+
+
+
+            return value;
+        }
+
+
+
+        public override string ToString()
+        {
+            string toString="";
+
+
+            return toString;
+        }
+
+
     }
 }
