9fbe42c88a1424d2a9e8a453432f73dc59d4ab10
brikedelai <pleblatier@live.fr>
2020-10-28 15:42:10 +0100

    add shop

--- Shop.cs
+++ Shop.cs
@@ -6,5 +6,48 @@ namespace Miniville
 {
     class Shop
     {
+<<<<<<< Updated upstream
+=======
+
+        List<Cards> ChampsBle = new List<Cards>();
+        List<Cards> Ferme = new List<Cards>();
+        List<Cards> Boulangerie = new List<Cards>();
+        List<Cards> Cafe = new List<Cards>();
+        List<Cards> Superette = new List<Cards>();
+        List<Cards> Foret = new List<Cards>();
+        List<Cards> Restaurant = new List<Cards>();
+        List<Cards> Stade = new List<Cards>();
+
+        List<List<Cards>> Shops = new List<List<Cards>>();
+
+        public List<List<Cards>> LigneAchat()
+        {
+            Shops.Add(ChampsBle);
+            Shops.Add(Ferme);
+            Shops.Add(Boulangerie);
+            Shops.Add(Cafe);
+            Shops.Add(Superette);
+            Shops.Add(Foret);
+            Shops.Add(Restaurant);
+            Shops.Add(Ferme);
+            Shops.Add(Stade);
+
+            return Shops;
+        }
+
+        public void AfficheShop()
+        {
+            System.Collections.IList list = Shops;
+            for (int i = 0; i < list.Count; i++)
+            {
+                List<string> subList = (List<string>)list[i];
+                //Console.WriteLine(subList);
+                foreach (string item in subList)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+>>>>>>> Stashed changes
     }
 }
