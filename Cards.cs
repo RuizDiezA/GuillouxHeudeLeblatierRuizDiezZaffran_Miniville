e6b1e01763c78834e39d5d837f1f02cd60b1b2c5
Sacha-Zaffran <73350628+Sacha-Zaffran@users.noreply.github.com>
2020-10-28 12:17:22 +0100

    Cards attributs + fix méthode

--- Cards.cs
+++ Cards.cs
@@ -9,16 +9,18 @@ namespace Miniville
         string color;
         int prix;
         List<int> CardsValue = new List<int>();
+        
         public Cards()
         {
             CardsValue.Add(1);
             CardsValue.Add(2);
             CardsValue.Add(4);
             CardsValue.Add(6);
+        }
+        
+        void CheckAndApplyEffect(int prix, string color)
+        {
 
-            void CheckAndApplyEffect(int prix, string color)
-            {
-            }
         }
     }
 }
