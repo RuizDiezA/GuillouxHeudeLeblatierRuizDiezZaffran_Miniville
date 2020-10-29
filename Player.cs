eddd190fa792a2ee0fe46d31e9713baae3fd50eb
LeCrampon <j.heude@yahoo.fr>
2020-10-29 09:47:02 +0100

    ReupdatePlayer

--- Player.cs
+++ Player.cs
@@ -15,8 +15,8 @@ namespace Miniville
         {
             this.money = 2;
             this.hand = new List<Cards>();
-            this.hand.Add(new Cards("Boulangerie", "green", 1, new List<int> { 2, 3 }, "Recevez 1 pièce"));
-            this.hand.Add(new Cards("Champs de blé", "blue", 1, new List<int> { 1 }, "Recevez 1 pièce"));
+            this.hand.Add(new Cards("Boulangerie", "green", 1, new List<int> { 2, 3 }, "Recevez 1 pièce", 0));
+            this.hand.Add(new Cards("Champs de blé", "blue", 1, new List<int> { 1 }, "Recevez 1 pièce", 0));
             this.name = "Player";
         }
 
